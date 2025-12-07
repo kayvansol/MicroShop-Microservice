f99ba04b4955343696283cae44c61e224ccb6513
kayvansol <kayvan.sol2@gmail.com>
2025-12-07 22:13:53 +0330

    add order status details needed for react project

--- Services/Order/MicroShop.Domain/DTOs/Order/GetAllOrders.cs
+++ Services/Order/MicroShop.Domain/DTOs/Order/GetAllOrders.cs
@@ -10,7 +10,8 @@ namespace MicroShop.Domain.DTOs.Order
     {
         public int OrderId { get; set; }
         public string CustomerName { get; set; }
-        public string OrderStatus { get; set; }
+        public string OrderStatusName { get; set; }
+        public byte OrderStatus { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime RequiredDate { get; set; }
         public DateTime? ShippedDate { get; set; }
